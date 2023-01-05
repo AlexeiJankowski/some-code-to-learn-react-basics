38e66fb42a40ea04f18be0b1ebb3df1f8fb77b03
AlexeiJankowski <alexey.cde@gmail.com>
2023-01-06 00:45:35 +0100

    Slider Added

--- README.md
+++ README.md
@@ -13,3 +13,10 @@ A calendar. I don't really like Date in JS, so it's a good training for me...
 ![Screenshot](screens/calendar.png)
 
 ## 03 What will it be?
+
+Kinda slider. It doesn't look amazing or even OK, because I've limited time for this element. 
+Photo was taken from [Rhamely] (https://unsplash.com/@rhamely?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText), like the colors
+
+![Screenshot](screens/slider.png)
+
+## 04 What will it be?
---
+++ slider/src/Slider.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+
+import './Slider.css';
+
+const images = [
+  {
+    path: "/img/1.jpg",
+    alt: "An croissant",
+    text: "Some Text"
+  },
+  {
+    path: "/img/2.jpg",
+    alt: "An croissant",
+    text: "Some Other Text"
+  }
+];
+
+const Slider = () => {
+  const [showSlide, setShowSlide] = useState(0);
+
+  console.log(showSlide);
+
+  return (
+    <section>
+      <Slide image={images[showSlide]} >
+        <Controls 
+          images={images}
+          setShowSlide={setShowSlide}
+        />
+      </Slide>
+    </section>
+  )
+}
+
+const Controls = ({images, setShowSlide}) => {
+  return (
+    <div className="btn-container">
+      {images.map((image, index) => {
+        return (
+          <button 
+            key={`button${index}`}
+            type="button"
+            onClick={() => setShowSlide(index)}
+          ></button>
+        )
+      })}
+      
+    </div>
+  )
+}
+
+const Slide = ({image, children}) => {
+  return (
+    <>
+      <figure>
+        <img src={image.path} alt={image.alt}/>
+        <figcaption>
+          <span>{image.text}</span>
+          {children}
+        </figcaption>
+      </figure>
+    </>
+  )
+}
+
+export default Slider;
